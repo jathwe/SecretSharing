84c24aadf4e08c62ee5caec19fc1fb932b6cf996
jathwe <54558016+jathwe@users.noreply.github.com>
2020-05-05 14:51:11 -0500

    deciding to use a hash on a string to get my big int, will be interesting

--- TrivSecrets.py
+++ TrivSecrets.py
@@ -1,5 +1,10 @@
 import hashlib
 
+def make_random_shares(shares):
+    secretString = "This is a trivial secret share implementation"
+    secret = abs(hash(s))**10%100000000000000000000000000000000
+    print (s1)
+
 def main():
     
 

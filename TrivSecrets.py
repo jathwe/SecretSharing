59cfbbd634e96964941034abc04fd1dc9556ea6d
jathwe <54558016+jathwe@users.noreply.github.com>
2020-05-05 16:40:49 -0500

    more work on trivial solution

--- TrivSecrets.py
+++ TrivSecrets.py
@@ -1,12 +1,17 @@
 import hashlib
 
-def make_random_shares(shares):
+def make_random_shares(numShares):
     secretString = "This is a trivial secret share implementation"
     secret = abs(hash(s))**10%100000000000000000000000000000000
-    print (s1)
+
+    shares = [2,3,4]
+    return secret, shares
 
 def main():
-    
+    numShares = 6
+    secret, shares = make_random_shares(numShares)
+    print (secret)
+    print (shares[1])
 
 
 if __name__ == '__main__':

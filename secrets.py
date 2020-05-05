1105f6ee283094f7e0c64b3d0fe2fe2c5eb6b229
jathwe <54558016+jathwe@users.noreply.github.com>
2020-05-05 14:41:50 -0500

    Recover and Lagrange

--- secrets.py
+++ secrets.py
@@ -65,6 +65,43 @@ def _divmod(num, den, p):
     inv, _ = _extended_gcd(den, p)
     return num * inv
 
+    def _lagrange_interpolate(x, x_s, y_s, p):
+        """
+    Finding the y values for the given x's,
+    k points with give us a polynomail of the kth order.
+    This is an implementation of lagrange interpolation
+    More information about that here:
+    https://brilliant.org/wiki/lagrange-interpolation/
+    """
+    k = len(x_s)
+    assert k == len(set(x_s)), "points must be distinct"
+    def PI(vals):  # upper-case PI -- product of inputs
+        accum = 1
+        for v in vals:
+            accum *= v
+        return accum
+    nums = []  # avoid inexact division
+    dens = []
+    for i in range(k):
+        others = list(x_s)
+        cur = others.pop(i)
+        nums.append(PI(x - o for o in others))
+        dens.append(PI(cur - o for o in others))
+    den = PI(dens)
+    num = sum([_divmod(nums[i] * den * y_s[i] % p, dens[i], p)
+               for i in range(k)])
+    return (_divmod(num, den, p) + p) % p
+
+    def recover_secret(shares, prime=_PRIME):
+        """
+    Recover the secrets using the points given via shares
+    """
+    #another error check
+    if len(shares) < 2:
+        raise ValueError("need at least two shares")
+    x_s, y_s = zip(*shares)
+    return _lagrange_interpolate(0, x_s, y_s, prime)
+
 def main():
     #Make the secret and its shares
     secret, shares = make_random_shares(minimum=4, shares=8)
